6f4a8bc2f98de6783e3fc5ed0ced471029e39085
MrMay0 <122040075+MrMay0@users.noreply.github.com>
2025-11-03 11:23:22 +0000

    [151] [Time Beats: 100.00%] [Memory Beats: 43.52%] - LeetPush

--- Reverse-Words-in-a-String.py
+++ Reverse-Words-in-a-String.py
@@ -1,15 +1,3 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        reversed = ''
-        n = len(s)
-        word = ''
-        for i in range(n-1,-1,-1):
-            if s[i] != ' ':
-                word += s[i]
-            elif word:
-                reversed += word[::-1].strip()
-                reversed += ' '
-                word = ''
-        if word:
-            reversed += word[::-1]
-        return reversed.strip()
+        return ' '.join(s.split()[::-1])
